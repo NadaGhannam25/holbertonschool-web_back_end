ecf3bbf9ebb066a579e28b2952733c0008a1287d
root <Nada.Ghannam19@gmail.com>
2026-02-18 05:13:55 +0000

    Add documented type-annotated to_str function

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Module that provides a function to convert a float to its string representation."""
 
-
 def to_str(n: float) -> str:
     """Return the string representation of the float n."""
     return str(n)
