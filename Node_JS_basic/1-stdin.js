91d479686dbc6f829b94cb5eb4bf080ad01fe45e
Nada Al-Mutairi <nada.ghannam19@gmail.com>
2026-04-09 22:35:37 +0300

    Change input event to readable for stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,8 +2,11 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (input) => {
-  process.stdout.write(`Your name is: ${input.trim()}\n`);
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
 process.stdin.on('end', () => {
